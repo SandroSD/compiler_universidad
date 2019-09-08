95e2043b2b221c9bc55e150fcb68708ade949fd6
Fernando <ferjalid@gmail.com>
2019-09-08 19:43:07 -0300

    agregado de comentarios y constantes al lexico, ademas de .h y .c de la ts

---
+++ 2019 - Primer Cuatrimestre/ts.h
@@ -0,0 +1,21 @@
+#ifndef TS_H
+#define TS_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "constantes.h"
+
+// TABLA DE SIMBOLOS
+struct struct_tablaSimbolos
+{
+    char nombre[100];
+    char tipo[100];
+    char valor[100];
+    char longitud[100];
+};
+struct struct_tablaSimbolos tablaSimbolos[10000];
+
+void insertarTokenEnTS(char *, char *);
+
+#endif
