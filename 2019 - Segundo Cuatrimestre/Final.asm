39e4002fe150bac2f1879b72a1e6a52e5f1ee02a
Fernando <ferjalid@gmail.com>
2019-11-30 21:39:08 -0300

    aa

--- 2019 - Segundo Cuatrimestre/Final.asm
+++ 2019 - Segundo Cuatrimestre/Final.asm
@@ -2,71 +2,17 @@
 ;ARCHIVO FINAL.ASM
 ;
 
-INCLUDE macros2.asm      ;incluye macros
-INCLUDE number.asm       ;incluye el asm para impresion de numeros
+INCLUDE macros2.asm		 ;incluye macros
+INCLUDE number.asm		 ;incluye el asm para impresion de numeros
 
 .MODEL LARGE ; tipo del modelo de memoria usado.
 .386
 .STACK 200h ; bytes en el stack
-<<<<<<< HEAD
-    
-.DATA        ; comienzo de la zona de datos.
-    TRUE equ 1
-    FALSE equ 0
-    MAXTEXTSIZE equ 30
-    R1 dd ?
-    @aux1 dd ?
-    @aux2 dd ?
-    @aux3 dd ?
-    @aux4 dd ?
-    @aux5 dd ?
-    @aux6 dd ?
-    @aux7 dd ?
-    @aux8 dd ?
-    @aux9 dd ?
-    @aux10 dd ?
-    @aux11 dd ?
-    @aux12 dd ?
-    @aux13 dd ?
-    @aux14 dd ?
-    @aux15 dd ?
-    _var1 dd 0
-    _var2 dd 0
-    _var3 dd 0
-    _var4 dd 0.0
-    _a dd 0
-    _a2 dd 0
-    _var7 dd 0
-    _valE dd 0
-    _2 dd 2
-    _valS dd 0
-    _Hola db "Hola mundo", '$', 18 dup(?)
-    _valR dd 0
-    _2_5 dd 2.5
-    _1 dd 1
-
-.CODE ;Comienzo de la zona de codigo
-START:       ;C?digo assembler resultante de compilar el programa fuente.
-    mov AX,@DATA         ;Inicializa el segmento de datos
-    mov DS,AX
-    ;ASIGNACION
-    fld _2
-    fstp _valE
-    ;ASIGNACION
-    fld _Hola
-    fstp _valS
-    ;ASIGNACION
-    fld _2_5
-    fstp _valR
-    ;ASIGNACION
-    fld _1
-    fstp _a2
-
 	
-.DATA ; comienzo de la zona de datos.
+.DATA		 ; comienzo de la zona de datos.
 	TRUE equ 1
 	FALSE equ 0
-	MAXTEXTSIZE equ 200
+	MAXTEXTSIZE equ 30
 	R1 dd ?
 	@aux1 dd ?
 	@aux2 dd ?
@@ -83,6 +29,17 @@ START:       ;C?digo assembler resultante de compilar el programa fuente.
 	@aux13 dd ?
 	@aux14 dd ?
 	@aux15 dd ?
+	var1 dd 0
+	var2 dd 0
+	var3 dd 0
+	var4 dd 0.0
+	a dd 0
+	a2 dd 0
+	var7 dd 0
+	&cte2 dd 2
+	&cte4 db "Hola mundo", '$', 18 dup(?)
+	&cte6 dd 2.5
+	&cte7 dd 1
 
 .CODE ;Comienzo de la zona de codigo
 START: 		 ;Código assembler resultante de compilar el programa fuente.
@@ -133,9 +90,8 @@ START: 		 ;Código assembler resultante de compilar el programa fuente.
 	fld @aux4
 	fstp _var1
 
-
-TERMINAR: ;Fin de ejecuci?n.
-    mov ax, 4C00h ; termina la ejecuci?n.
-    int 21h; syscall
+TERMINAR: ;Fin de ejecución.
+	mov ax, 4C00h ; termina la ejecución.
+	int 21h; syscall
 
 END START;final del archivo.
